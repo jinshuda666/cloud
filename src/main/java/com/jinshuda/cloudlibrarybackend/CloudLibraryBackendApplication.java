36202ec170e4603702ee1963f520806f5f340997
晋树达 <2664104891@qq.com>
2024-12-10 15:59:42 +0800

    后端模板

---
+++ src/main/java/com/jinshuda/cloudlibrarybackend/CloudLibraryBackendApplication.java
@@ -0,0 +1,17 @@
+package com.jinshuda.cloudlibrarybackend;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@MapperScan("com.jinshuda.cloudlibrarybackend.mapper")
+@EnableAspectJAutoProxy(exposeProxy = true)
+public class CloudLibraryBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudLibraryBackendApplication.class, args);
+    }
+
+}
